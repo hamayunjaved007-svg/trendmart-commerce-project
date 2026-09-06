6b02c206c65db56bbb05eeb194c420fab234acfe
Hamayun Javed <hamayunjaved007@gmail.com>
2026-09-06 19:45:51 +0500

    register without database

--- actions/signup.php
+++ actions/signup.php
@@ -3,13 +3,13 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $cpassword = $_POST['confirm_password'];    
-    if($password == $cpassword){
-        $cn = mysqli_connect("localhost","root","","trendmart") or die('cant connect with database..!');
-        $query = "INSERT INTO `users`(name,email,password) VALUES ('$name','$email','$password')";
-      //  var_dump($query);
-        mysqli_query($cn,$query) or die('cant run query');
-        echo "Account Created Successfully";
-    } else {
-        echo "Password Not Matched";
-    }
+    // if($password == $cpassword){
+    //     $cn = mysqli_connect("localhost","root","","trendmart") or die('cant connect with database..!');
+    //     $query = "INSERT INTO `users`(name,email,password) VALUES ('$name','$email','$password')";
+    //   //  var_dump($query);
+    //     mysqli_query($cn,$query) or die('cant run query');
+    //     echo "Account Created Successfully";
+    // } else {
+    //     echo "Password Not Matched";
+    // }
 ?>
